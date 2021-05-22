2e5bdc6e13f6083814d230a6e76bdfcb9d2a8347
Alex Coronell <alexcoronell@gmail.com>
2021-05-22 11:18:36 -0500

    Uso de spinner en characters

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -3,21 +3,43 @@ import React, { useState, useEffect } from 'react'
 // Componentes
 import Character from './Character';
 import Pagination from './Pagination';
+import { CircularProgress } from '@material-ui/core'
 
 // Servicios
 import getCharacters from './../services/getCharacters';
+
+const INITIAL_PAGE = 0;
 
 const Characters = () => {
 
     const [characterList, setCharacterList] = useState([]);
 
+    const [loading, setLoading] = useState(false)
+
+    const [page, setPage] = useState(INITIAL_PAGE);
+
     useEffect(() => {
-        getCharacters().then(characters => setCharacterList(characters));
+        setLoading(true)
+        getCharacters()
+            .then(characters => setCharacterList(characters))
+            .then(() => {
+                setTimeout(() => setLoading(false), 500);
+            });
     }, [])
 
+    useEffect(() => {
+        
+    }, [page])
+
     return (
+        
         <main className="characters container-fluid">
-            <h1>Characters</h1>
+           { loading
+           ? <> <h1>Characters</h1>
+                <CircularProgress />
+            </>
+           : <>
+                <h1>Characters</h1>
             <section className="characters__list row">
             {
                 characterList.map(({char_id, img, name, nickname}) => {
@@ -34,6 +56,8 @@ const Characters = () => {
             }
             </section>
             <Pagination />
+            </>
+            }
         </main>
     )
 }
