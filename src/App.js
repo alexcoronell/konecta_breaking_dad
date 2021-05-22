c166877a41fd08154e2bbabd7466ffdc9b0bc30c
Alex Coronell <alexcoronell@gmail.com>
2021-05-21 23:02:24 -0500

    Maquetación básica de characters

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
+import Characters from './components/Characters';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Characters />
     </div>
   );
 }
---
+++ src/components/Characters.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react'
+import Character from './Character';
+
+const Characters = () => {
+
+    const [characterList, setCharacterList] = useState([]);
+
+    useEffect(() => {
+        fetch('https://www.breakingbadapi.com/api/characters')
+            .then(res => res.json())
+            .then(res => setCharacterList(res))
+    }, [])
+
+    return (
+        <main className="characters container-fluid">
+            <h1>Characters</h1>
+            <section className="characters__list row">
+            {
+                characterList.map(({char_id, img, name, nickname, birthday, occupation}) => {
+                    return (
+                        <Character
+                            char_id = {char_id}
+                            img = {img}
+                            name = {name}
+                            nickname = {nickname}
+                            birthday = {birthday}
+                            occupation = {occupation}
+                        />
+                    )
+                })
+            }
+            </section>
+        </main>
+    )
+}
+
+export default Characters
